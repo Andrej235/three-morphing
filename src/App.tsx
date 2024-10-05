d761714af5ef8b48cbf968031e2921be623458a0
Andrej Nenadić <andrejnana445@gmail.com>
2024-10-05 22:54:44 +0200

    Animate morphing between 2 morph targets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import MorphingElement from "./MorphingElement";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <Canvas>
       <MorphingElement />
+      <OrbitControls />
     </Canvas>
   );
 }
--- src/MorphingElement.tsx
+++ src/MorphingElement.tsx
@@ -1,3 +1,35 @@
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Mesh, MeshBasicMaterial } from "three";
+
 export default function MorphingElement() {
-  return <mesh></mesh>;
+  const {
+    scene: {
+      children: [element],
+    },
+  } = useGLTF("morphing.glb");
+
+  const { scene } = useThree();
+  const meshRef = useRef<Mesh | null>(null);
+
+  useEffect(() => {
+    if (!element) return;
+
+    meshRef.current = element as Mesh;
+    meshRef.current.material = new MeshBasicMaterial({
+      color: "#fff",
+    });
+
+    scene.add(meshRef.current);
+  }, [scene, element]);
+
+  useFrame(({ clock: { elapsedTime: time } }) => {
+    if (!meshRef.current?.morphTargetInfluences) return;
+
+    console.log(meshRef.current?.morphTargetInfluences);
+    meshRef.current.morphTargetInfluences[0] = Math.sin(time * 2) * 0.5 + 0.5;
+  });
+
+  return <></>;
 }
