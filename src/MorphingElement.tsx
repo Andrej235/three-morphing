174f77fd3df0c327f030dc01c3b9859ec4e7594a
Andrej Nenadić <andrejnana445@gmail.com>
2024-10-06 12:04:07 +0200

    Add dat.GUI and use it to display and modify values of morph target influences

--- src/MorphingElement.tsx
+++ src/MorphingElement.tsx
@@ -1,7 +1,8 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Mesh, MeshBasicMaterial } from "three";
+import * as dat from "dat.gui";
 
 export default function MorphingElement() {
   const {
@@ -12,6 +13,8 @@ export default function MorphingElement() {
 
   const { scene } = useThree();
   const meshRef = useRef<Mesh | null>(null);
+  const guiRef = useRef<dat.GUI | null>(null);
+  const [runAnimation, setRunAnimation] = useState(false);
 
   useEffect(() => {
     if (!element) return;
@@ -24,11 +27,29 @@ export default function MorphingElement() {
     scene.add(meshRef.current);
   }, [scene, element]);
 
-  useFrame(({ clock: { elapsedTime: time } }) => {
+  useEffect(() => {
     if (!meshRef.current?.morphTargetInfluences) return;
 
-    console.log(meshRef.current?.morphTargetInfluences);
+    if (guiRef.current) guiRef.current.destroy();
+
+    guiRef.current = new dat.GUI();
+    for (let i = 0; i < meshRef.current.morphTargetInfluences.length; i++)
+      guiRef.current.add(meshRef.current.morphTargetInfluences, i, 0, 1, 0.01);
+
+    guiRef.current.add(
+      {
+        ToggleAnimation: () => void setRunAnimation(!runAnimation),
+      },
+      "ToggleAnimation"
+    );
+  }, [meshRef.current, runAnimation]);
+
+  useFrame(({ clock: { elapsedTime: time } }) => {
+    if (!meshRef.current?.morphTargetInfluences || !runAnimation) return;
+
     meshRef.current.morphTargetInfluences[0] = Math.sin(time * 2) * 0.5 + 0.5;
+    meshRef.current.morphTargetInfluences[1] =
+      1 - (Math.sin(time * 2) * 0.5 + 0.5);
   });
 
   return <></>;
